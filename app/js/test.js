84fe71b2d2c0e933d5941b573169f818f514800e
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-09-15 23:08:40 +0300

    first commit

---
+++ app/js/test.js
@@ -0,0 +1,6 @@
+export default function test(name = 'noname') {
+// $(document).ready(function(){
+//   alert('Ваша версия jQuery' + jQuery.fn.jquery);
+// });
+  console.log('WebPack is ready');
+}
