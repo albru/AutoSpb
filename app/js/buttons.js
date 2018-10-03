4d7936e7b804ff9c5566f986032700900bdeb5a3
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-10-03 23:19:51 +0300

    features list

--- app/js/buttons.js
+++ app/js/buttons.js
@@ -32,11 +32,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showPrivacy(greetingsPrivacy, greetingsPrivacyText)
 showPrivacy(popupPrivacy, popupPrivacyText)
 
-
-
-
-
-
   const popupButtons = (function (params) {
     const popupBtns = document.querySelectorAll('.js-popup-btn');
     const popupBtnsArr = Array.from(popupBtns);
