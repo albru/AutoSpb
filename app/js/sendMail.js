9f76c1c2b1bfdcf1a89da46e52bba29c0fd60038
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-10-25 00:29:27 +0300

    show loading

--- app/js/sendMail.js
+++ app/js/sendMail.js
@@ -3,8 +3,10 @@ const popupElem = $('.popup__DoneJS');
 const formElem = $("#submit-form");
 const popupElem1 = $('.popup__DoneJS1');
 const formElem1 = $("#submit-form1");
+const speedometer = $('.gauge-loader-cont');
+let speedometerNumber = 0;
 
-function submitForm(form, pop) {
+function submitForm(form, pop, speedometerNumber) {
   $(document).ready(function () {
     const popup = pop;
     const popupFunc = function () {
@@ -17,22 +19,29 @@ function submitForm(form, pop) {
       });
     };
 
-    form.submit(function () { //Change
+    form.submit(function (e) { //Change
+      e.preventDefault();
+      speedometer[speedometerNumber].style.opacity = 1;
+      speedometer[speedometerNumber].style.display = 'initial';
       const th = $(this);
       $.ajax({
         type: "POST",
         url: "server.php", //Change
         data: th.serialize()
       }).done(function () {
+        speedometer[speedometerNumber].style.opacity = 0;
+        speedometer[speedometerNumber].style.display = 'none';
         popupFunc();
         setTimeout(function () {
           th.trigger("reset");
         }, 1000);
+      }).catch((error) => {
+        error.statusText ? console.log(error.statusText) : null;
       });
       return false;
     });
   });
 };
 
-submitForm(formElem, popupElem);
-submitForm(formElem1, popupElem1);
+submitForm(formElem, popupElem, 1);
+submitForm(formElem1, popupElem1, 0);
